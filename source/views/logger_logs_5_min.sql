b4ed45db793e077b377120f192315a6a1b4c253b
Alex Nuijten <alexnuijten@gmail.com>
2026-09-01 10:24:54 +0200

    Version 3.2.1: decrease indentation for Call_tree in logger_logs_5_min and logger_logs_60_min

--- source/views/logger_logs_5_min.sql
+++ source/views/logger_logs_5_min.sql
@@ -1,8 +1,10 @@
 create or replace force view logger_logs_5_min as
   select
     ll.*,
-    lpad(' ', greatest(nvl(call_depth,1)-1,0) * 3) ||
-      case when nvl(call_depth,1) > 1 then '|-- ' end ||
+    lpad(' ', greatest(nvl(call_depth,1) -
+      min(nvl(call_depth,1)) over (partition by call_id), 0) * 3) ||
+      case when nvl(call_depth,1) >
+        min(nvl(call_depth,1)) over (partition by call_id) then '|-- ' end ||
       unit_name as call_tree
   from logger_logs ll
  where time_stamp > systimestamp - (5/1440)
